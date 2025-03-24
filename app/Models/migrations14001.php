e1e8addf566efe1f14e062338aaa26479df211f4
maqilfahmi <111202214001.mhs.dinus.ac.id>
2025-03-25 05:51:43 +0700

    migration commit

---
+++ app/Models/migrations14001.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class migrations14001 extends Model
+{
+    use HasFactory;
+}
